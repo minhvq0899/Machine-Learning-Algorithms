e2e92e62fbaa871babe20f102d823ee0bdf24bef
Minh Quang Vu <minhvq0899@gmail.com>
2020-11-13 23:19:08 +0700

    scikit

--- linreg/multilinear-scikit.py
+++ linreg/multilinear-scikit.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append("C:\\Users\\minhv\\Machine-Learning-Algorithms\\linreg")
 
 from multilinear_Normal_Equation import *
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model   import LinearRegression
 
 # fit the model by Linear Regression
 reg = LinearRegression(fit_intercept=False) # fit_intercept = False for calculating the bias
